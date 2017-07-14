ae69d07033af273be93a1d17c2e25e61dd7724ae
perlancar (on pc-office) <perlancar@gmail.com>
2017-07-14 22:41:42 +0700

    Release v0.001

---
+++ lib/App/cpanm/perlancar.pm
@@ -0,0 +1,19 @@
+package App::cpanm::perlancar;
+
+# DATE
+# VERSION
+
+1;
+
+# ABSTRACT: Install modules from CPAN (PERLANCAR's flavor)
+
+=head1 DESCRIPTION
+
+See the included L<cpanm-perlancar> script.
+
+
+=head1 SEE ALSO
+
+L<App::cpanminus>
+
+=cut
